fc24377f5bb723c3da13f8b356b082c1fb0490a2
Mohamed-Amiri <mohamedamiri281@gmail.com>
2025-02-17 09:39:47 +0100

    Update SQL Worqshop.sql

--- SQL Worqshop.sql
+++ SQL Worqshop.sql
@@ -1,8 +1,40 @@
 create table  Avion  (
-AvionID int,
-AvionNom varchar(255),
-AvionCapacite int,
-AvionLocalite varchar (255)
-)
+ID int primary key,
+Nom varchar(255),
+Capacite int,
+Localite varchar (255)
+
+);
+
+create table  Pilote (
+ID int PRIMARY KEY ,
+Nom varchar(255),
+Adresse varchar(255)
+
+);
+
+DROP table IF EXISTS vol;
+create table vol (
+	ID int PRIMARY KEY,
+	PiloteID int,
+	AvionID int, 
+	Depart varchar(255),
+	Arive varchar(255), 
+	HeureDépart int,
+	HeurArrive int,
+	FOREIGN KEY (AvionID) REFERENCES Avion(ID),
+	FOREIGN KEY (PiloteID) REFERENCES Pilote(ID)
+
+);
+
+INSERT INTO Avion (ID, nom, Capacite, Localite)
+VALUES 
+   (100, 'AIRBUS',  300, 'RABAT'),
+   (101, 'B737',    250, 'CASA'),
+   (102, 'B737',    150, 'RABAT');
+   SELECT * FROM Avion;
+   
+SELECT * FROM  Avion 
+ORDER BY nom DESC;
 
 
